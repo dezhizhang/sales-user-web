d2b4d4f813bfddb4c35ad95103f69049f32e1f65
dezhizhang <1541609448@qq.com>
2022-07-09 21:22:02 +0800

    feat:grpc

--- api/user.go
+++ api/user.go
@@ -1,10 +1,26 @@
 package api
 
 import (
-	"fmt"
+	"context"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"net/http"
+	"user_web/proto"
 )
 
 func GetUserList(ctx *gin.Context) {
-	fmt.Println("hello")
+	conn, err := grpc.Dial("127.0.0.1:8000", grpc.WithInsecure())
+	if err != nil {
+		zap.S().Errorw("连接服务失败", "msg", err.Error())
+	}
+	//proto.UserClient()
+	userSrvClient := proto.NewUserClient(conn)
+	rsp, err := userSrvClient.GetUserList(context.Background(), &proto.PageInfo{PageSize: 10, PageIndex: 1})
+	if err != nil {
+		zap.S().Errorw("查询用户列表失败")
+		return
+	}
+	ctx.JSON(http.StatusOK, rsp)
+	zap.S().Debug("获取用户列表")
 }
--- go.mod
+++ go.mod
@@ -30,6 +30,8 @@ require (
 	golang.org/x/net v0.0.0-20220630215102-69896b714898 // indirect
 	golang.org/x/sys v0.0.0-20220704084225-05e143d24a9e // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.47.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )

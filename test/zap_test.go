adeec8d055ac8cf28bd97154abb1b27e6ccf0004
dezhizhang <1541609448@qq.com>
2023-09-01 23:31:53 +0800

    feat:zap日志

--- test/zap_test.go
+++ test/zap_test.go
@@ -2,7 +2,9 @@ package test
 
 import (
 	"go.uber.org/zap"
+	"log"
 	"testing"
+	"time"
 )
 
 func TestZap(t *testing.T) {
@@ -14,4 +16,24 @@ func TestZap(t *testing.T) {
 	sugar := logger.Sugar()
 	sugar.Infow("failed to fetch URL", "url", url, "attempt", 3)
 	sugar.Infof("Failed to fetch URL:%s", url)
+}
+
+func TestZapFile(t *testing.T) {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{
+		"./myproject.log",
+	}
+	logger, err := config.Build()
+	if err != nil {
+		log.Printf("写入日志失败%s", err.Error())
+	}
+	su := logger.Sugar()
+	defer logger.Sync()
+
+	url := "https://imooc.com"
+	su.Info("failed to fetch URL",
+		zap.String("url", url),
+		zap.Int("attempt", 3),
+		zap.Duration("backoff", time.Second),
+	)
 }
